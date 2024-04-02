35b34761091ac332b90222301ca0f96c42712e1a
Petrică <120740177+PetricaH@users.noreply.github.com>
2024-04-02 10:14:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // menu
-function toggleMenu(element) {
+/* function toggleMenu(element) {
     element.classList.toggle("change");
     var menuLinks = document.querySelector('.menu-links');
     menuLinks.classList.toggle("show");
@@ -13,6 +13,30 @@ document.querySelectorAll('.menu-links a').forEach(function(link) {
         toggleMenu(menuButton); // this closes the menu by toggling the menu button
     });
 });
+*/
+
+function toggleMenu(element) {
+  if (window.innerWidth < 930) {
+    element.classList.toggle('change');
+    var menuLinks = document.querySelector('.menu-links');
+    menuLinks.classList.toggle('show');
+
+    document.body.style.overflow = (menuLinks.classList.contains('show')) ? 'hidden' : 'auto';
+  }
+}
+
+document.querySelectorAll('.menu-links a').forEach(function(link) {
+  link.addEventListener('click', function() {
+    var menuButton = document.querySelector('.menu');
+    if (window.innerWidth < 930) {
+      toggleMenu(menuButton);
+    }
+  });
+});
+
+
+
+
 
 const cardsContainer = document.querySelector('.cards-part-specializari');
 const cursor = document.querySelector(".cursor");
